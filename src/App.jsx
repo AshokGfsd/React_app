95a2aa4cb68de8213cded6c9407cc9514a9eb9d2
AshokGfsd <166788378+AshokGfsd@users.noreply.github.com>
2024-05-27 10:29:01 +0530

    a2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import Hello from "./components/Hello"
-function App() {
-  return (
-    <>
-    <h1>Greetings</h1>
-    <Hello/>
-    </>
-  )
+import React, { Component } from 'react';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props); // Call the constructor of the parent class
+
+    // set the initial state
+    this.state = {
+      counter: 0
+    }
+  }
+
+  handleIncrement = () => {
+    
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+  handleDecrement = () => {
+    if(this.state.counter !=0){
+    this.setState({
+      counter: this.state.counter - 1
+    });}
+  }
+  render() {
+
+    console.log(this.state);
+
+    return (
+      <div>
+        <p>Count: {this.state.counter}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    )
+  }
 }
 
-export default App
+export default App;
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,9 @@
-const Hello = (data) => {
-  return <p>HELLO {data.name}</p>;
-};
+import { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return <h1>My React App</h1>;
+  }
+}
 
 export default Hello
